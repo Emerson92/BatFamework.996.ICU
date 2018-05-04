3315b3b12a35f773392ae2939dc9834454f7ed17
DESKTOP-PN329TK\TheDarkKngiht <846751908@qq.com>
2018-05-04 17:23:49 +0800

    ^#添加对生命周期添加优先级与生命周期管控类

--- Assets/Script/BatCore/LifeCycleControl.cs
+++ Assets/Script/BatCore/LifeCycleControl.cs
@@ -10,39 +10,45 @@ namespace THEDARKKNIGHT
 
         private static List<ILifeCycle> ItemList = new List<ILifeCycle>();
 
-        public static void Add(ILifeCycle i) {
+        public static void Add(ILifeCycle i)
+        {
             ItemList.ForEach((ILifeCycle item) =>
             {
-                if (item == i) {
+                if (item == i)
+                {
                     i = null;
                     return;
                 }
             });
             if (i != null)
-               ItemList.Add(i);
+                ItemList.Add(i);
         }
 
-        public static void Remove(ILifeCycle i) {
-             ItemList.Remove(i);
+        public static void Remove(ILifeCycle i)
+        {
+            ItemList.Remove(i);
         }
 
         public void Start(MonoBehaviour enter)
         {
-            LoopSend((ILifeCycle i) => {
+            LoopSend((ILifeCycle i) =>
+            {
                 i.BStart(enter);
             });
         }
 
         public void Awake(MonoBehaviour enter)
         {
-            LoopSend((ILifeCycle i) => {
+            LoopSend((ILifeCycle i) =>
+            {
                 i.BAwake(enter);
             });
         }
 
         public void Update(MonoBehaviour enter)
         {
-            LoopSend((ILifeCycle i) => {
+            LoopSend((ILifeCycle i) =>
+            {
                 i.BUpdate(enter);
             });
         }
@@ -53,6 +59,48 @@ namespace THEDARKKNIGHT
             {
                 fuction(i);
             });
+        }
+    }
+
+    public class LifeCycleTool
+    {
+
+        Dictionary<LifeType, bool> LifeStatue = new Dictionary<LifeType, bool>() {
+            { LifeType.Awake,false},
+            { LifeType.Start,false},
+            { LifeType.Update,false},
+            { LifeType.FixedUpdate,false},
+            { LifeType.LateUpdate,false},
+            { LifeType.OnApplicationFocus,false},
+            { LifeType.OnApplicationPause,false},
+            { LifeType.OnApplicationQuit,false},
+            { LifeType.OnDestroy,false},
+            { LifeType.OnDisable,false},
+            { LifeType.OnEnable,false}
+        };
+
+        public enum LifeType
+        {
+            Awake,
+            Start,
+            Update,
+            FixedUpdate,
+            LateUpdate,
+            OnApplicationFocus,
+            OnApplicationPause,
+            OnApplicationQuit,
+            OnDestroy,
+            OnDisable,
+            OnEnable
+        }
+
+        ILifeCycle Icycle;
+
+        int priority;
+
+        public LifeCycleTool SetLifeCycle(LifeType type,bool excute) {
+            LifeStatue[type] = excute;
+            return this;
         }
     }
 }
