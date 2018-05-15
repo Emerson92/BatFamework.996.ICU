32338a3304aa5d3333df44f26f8adb21f178c4ef
DESKTOP-PN329TK\TheDarkKngiht <846751908@qq.com>
2018-05-15 13:56:11 +0800

    ^#添加文件操作

---
+++ Assets/Script/FileSystem/BFileSystem.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+namespace THEDARKKNIGHT.FileSystem
+{
+    public class BFileSystem : BatSingletion<BFileSystem>
+    {
+
+        private BFileSystem() { }
+
+        public FileStream CreateFile(string path, string fileName)
+        {
+            FileStream file = null;
+            try
+            {
+                file = File.Create(path + "/" + fileName);
+            }
+            catch (Exception ex)
+            {
+                BLog.Instance().Log(ex.Message);
+            }
+
+            return file;
+        }
+
+        public FileStream OpenFile(string path, string FileName)
+        {
+            FileStream file = null;
+            try
+            {
+                file = OpenFile(path + "/" + FileName);
+            }
+            catch (Exception ex)
+            {
+                BLog.Instance().Log(ex.Message);
+            }
+            return file;
+        }
+
+        public FileStream OpenFile(string fullPath)
+        {
+            FileStream file = null;
+            try
+            {
+                file = File.Open(fullPath, FileMode.Append);
+            }
+            catch (Exception ex) {
+                BLog.Instance().Log(ex.Message);
+            }
+            
+            return file;
+        }
+
+        public void DeleteFile(string fullpath, bool deleteAll = false)
+        {
+            try
+            {
+                File.Delete(fullpath);
+            }
+            catch (Exception ex)
+            {
+                BLog.Instance().Log(ex.Message);
+            }
+
+        }
+
+        public void MoveFile(string targetPath, string destinationPath)
+        {
+            try
+            {
+                File.Move(targetPath, destinationPath);
+            }
+            catch (Exception ex) {
+                BLog.Instance().Log(ex.Message);
+            }
+           
+        }
+
+        public void CopyFile(string targetPath, string destinationPath)
+        {
+            try
+            {
+                File.Copy(targetPath, destinationPath);
+            }
+            catch (Exception ex) {
+                BLog.Instance().Log(ex.Message);
+            }
+        }
+    }
+}
