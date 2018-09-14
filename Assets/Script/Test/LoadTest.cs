3f47948ce30157113baad3327c7e54dbe4630ac2
DESKTOP-BS5JE8B\TheDarkKnight <846571908@qq.com>
2018-09-14 17:55:21 +0800

    update loadScene and Test Script!!!!

---
+++ Assets/Script/Test/LoadTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using THEDARKKNIGHT.ConstDefine;
+using THEDARKKNIGHT.ResourceSystem;
+using UnityEngine;
+
+public class LoadTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BResourceMgr.Instance().Init();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            Debug.Log(BFameWorkDefine.BFameDataPath + "/dungouji.sdf");
+            BResourceMgr.Instance().LoadCacheResAsync(BFameWorkDefine.BFameDataPath + "/dungouji.sdf", (AssetBundleCreateRequest request) => {
+                GameObject DunGunJI = request.assetBundle.LoadAsset<GameObject>("dungouji") as GameObject;
+                Instantiate(DunGunJI);
+            });
+
+        }
+    }
+}
