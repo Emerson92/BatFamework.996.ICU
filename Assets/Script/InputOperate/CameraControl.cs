c6a24bb1df808d354921c2bbd6de834fed95b992
徐雨同 <846751908@qq.com>
2018-09-25 22:29:01 +0800

    Add the Camera Operate Script

---
+++ Assets/Script/InputOperate/CameraControl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+///  Camera control Script 
+///  this is script main to control the Camera status on the scene
+///  such as the distance and rotation
+/// </summary>
+public class CameraControl {
+
+}
