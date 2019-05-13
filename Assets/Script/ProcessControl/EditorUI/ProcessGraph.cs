6ca7f88850209b4908abbf0a5f053a627b57b9ba
Emerson92 <846751908@qq.com>
2019-05-13 18:10:13 +0800

    添加可视化流程编辑界面

---
+++ Assets/Script/ProcessControl/EditorUI/ProcessGraph.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+namespace THEDARKKNIGHT.ProcessCore.Graph {
+
+    [CreateAssetMenu]
+    public class ProcessGraph : XNode.NodeGraph
+    {
+
+    }
+
+    [Serializable]
+    public class ProcessPort { }
+}
---
+++ Assets/Script/ProcessControl/EditorUI/ProcessItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using THEDARKKNIGHT.ProcessCore;
+using UnityEngine;
+using XNode;
+namespace THEDARKKNIGHT.ProcessCore.Graph {
+   
+    public class ProcessItem : XNode.Node
+    {
+        [Input]
+        public ProcessPort EnterProcess;
+
+        [Output]
+        public ProcessPort outPortProcess;
+
+        public ClassType[] ProcessItems;
+
+        public ProcessPort OutPortProcess
+        {
+            get
+            {
+                return outPortProcess;
+            }
+
+            set
+            {
+                outPortProcess = value;
+            }
+        }
+
+        // Use this for initialization
+        protected override void Init()
+        {
+            base.Init();
+
+        }
+
+        // Return the correct value of an output port when requested
+        public override object GetValue(NodePort port)
+        {
+            return null; // Replace this
+        }
+    }
+
+    [Serializable]
+    public class ClassType {
+
+        public string className;
+
+        public string Namespace;
+
+        public BProcessItem item;
+    }
+}
